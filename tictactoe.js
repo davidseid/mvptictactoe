cc1acf7a6b0edb361543128977173ef01783e1c4
David Seidenberg <davidseidenberg@gmail.com>
2018-04-11 14:07:13 -0700

    Display the player turn

--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,32 @@
 // use minimist and inquirer to get input from the command line
 
 // build basic board structure
-const board = [['x', ' ', 'o'], [' ', 'o', ' '], [' ', ' ', 'x']];
+const board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
 
-console.log(`${board[0][0]} | ${board[0][1]} | ${board[0][2]}`);
-console.log('---------');
-console.log(`${board[1][0]} | ${board[1][1]} | ${board[1][2]}`);
-console.log('---------');
-console.log(`${board[2][0]} | ${board[2][1]} | ${board[2][2]}`);
+let player1Turn = true;
+let player;
+
+if (player1Turn === true) {
+  player = 'player1';
+} else {
+  player = 'player2';
+}
+
+const printBoard = () => {
+  console.log(`${board[0][0]} | ${board[0][1]} | ${board[0][2]}`);
+  console.log('---------');
+  console.log(`${board[1][0]} | ${board[1][1]} | ${board[1][2]}`);
+  console.log('---------');
+  console.log(`${board[2][0]} | ${board[2][1]} | ${board[2][2]}`);
+  console.log(`${player}, it is your turn`);
+};
+
+printBoard();
+
+
+
+
+
 
 // display the current board and who's turn it is
 
