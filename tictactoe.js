e757efbe39de1a5c0ef6ef8a29560322ddbe19db
David Seidenberg <davidseidenberg@gmail.com>
2018-04-11 14:02:27 -0700

    Create basic board display

---
+++ tictactoe.js
@@ -0,0 +1,16 @@
+// use minimist and inquirer to get input from the command line
+
+// build basic board structure
+const board = [['x', ' ', 'o'], [' ', 'o', ' '], [' ', ' ', 'x']];
+
+console.log(`${board[0][0]} | ${board[0][1]} | ${board[0][2]}`);
+console.log('---------');
+console.log(`${board[1][0]} | ${board[1][1]} | ${board[1][2]}`);
+console.log('---------');
+console.log(`${board[2][0]} | ${board[2][1]} | ${board[2][2]}`);
+
+// display the current board and who's turn it is
+
+// display options for choosing the next move 
+
+// create win logic
